f23ddc62527b47aaa97d133ded47f19b1780907e
Muh Arsyal Afifan Ersaputra <ersaputra21@gmail.com>
2023-03-10 14:30:04 +0700

    configuration react router dom in landing page

---
+++ src/route/RouteConfiguration.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const RouteConfiguration = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default RouteConfiguration;
